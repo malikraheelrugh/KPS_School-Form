850400bdef0891cbbece7ba39a32ff58aea7ab12
malikraheelrugh <raheelrugh677@gmail.com>
2026-06-21 08:07:00 +0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Allow frontend requests during development.
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "*",
+    origin: process.env.Frontend_Url || "*",
   })
 );
 app.use(express.json());
